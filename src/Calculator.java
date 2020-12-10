04a5bc560d0e24876ae7e79ba26677758ccbb82e
howdyAnkit <palankit35@gmail.com>
2020-12-10 19:29:50 +0530

    Setting Baisc Working Directory

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+
+public class Calculator {
+	
+	public int calculate(String input) {
+		if(isEmpty(input)) {
+			return 0;
+		}
+		return stringToInt(input);
+	}
+	
+	private boolean isEmpty(String input) {
+		return input.isEmpty();
+	}
+	
+	private int stringToInt(String input) {
+		return Integer.parseInt(input);
+	}
+}
---
+++ src/CalculatorTest.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+@Test
+public class CalculatorTest {
+	
+	private Calculator calculator;
+	
+	@BeforeTest
+	public void init() {
+		calculator = new Calculator();
+	}
+	
+	public void emptyStringReturnsZero() {
+		assertEquals(calculator.calculate(""),0);
+	}
+	
+	public void singleValueIsReplied() {
+		assertEquals(calculator.calculate("1"),1);
+	}
+}
